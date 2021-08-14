d58cf7191814bf70512a20cc03f21b56bc8542c6
Jawed Alam <zjaweds@outlook.com>
2021-08-15 00:09:44 +0530

    Only Face detection implementation

--- faceDetection.py
+++ faceDetection.py
@@ -13,18 +13,19 @@ videoCapture = cv.VideoCapture(0)
 while True:
     ret, frame = videoCapture.read()
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(gray, scaleFactor= 1.1, minNeighbors = 5, minSize = (60,60), flags = cv.CASCADE_SCALE_IMAGE)
+    faces = faceCascade.detectMultiScale(gray,
+                                        scaleFactor= 1.1, 
+                                        minNeighbors = 5, 
+                                        minSize = (60,60), 
+                                        flags = cv.CASCADE_SCALE_IMAGE)
     for (x,y,w,h) in faces:
         cv.rectangle(frame, (x,y),(x+w, y+h), (0,255,0),2)
         faceROI = frame[y:y+h,x:x+w]
         eyes = eyeCascade.detectMultiScale(faceROI)
-        for (x2, y2, w2, h2) in eyes:
-            eye_center = (x + x2 + w2 // 2, y + y2 + h2 // 2)
-            radius = int(round((w2 + h2) * 0.25))
-            frame = cv.circle(frame, eye_center, radius, (255, 0, 0), 4)
+        
     cv.imshow('Video',frame)
     # cv.waitKey()
     if cv.waitKey(1) &0xFF == ord('q'):
         break
-    videoCapture.release()
-    cv.destroyAllWindows()
+videoCapture.release()
+cv.destroyAllWindows()
