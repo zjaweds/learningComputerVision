05ee223bee517aea07e006f8063457b70688002c
Jawed Alam <zjaweds@outlook.com>
2021-08-15 00:12:05 +0530

    Only eye detection minimal implementation

--- faceDetection.py
+++ faceDetection.py
@@ -19,10 +19,14 @@ while True:
                                         minSize = (60,60), 
                                         flags = cv.CASCADE_SCALE_IMAGE)
     for (x,y,w,h) in faces:
-        cv.rectangle(frame, (x,y),(x+w, y+h), (0,255,0),2)
-        
+        # cv.rectangle(frame, (x,y),(x+w, y+h), (0,255,0),2)
+        faceROI = frame[y:y+h,x:x+w]
+        eyes = eyeCascade.detectMultiScale(faceROI)
+        for (x2, y2, w2, h2) in eyes:
+            eye_center = (x + x2 + w2 // 2, y + y2 + h2 // 2)
+            radius = int(round((w2 + h2) * 0.25))
+            frame = cv.circle(frame, eye_center, radius, (255, 0, 0), 4)
     cv.imshow('Video',frame)
-    # cv.waitKey()
     if cv.waitKey(1) &0xFF == ord('q'):
         break
 videoCapture.release()
