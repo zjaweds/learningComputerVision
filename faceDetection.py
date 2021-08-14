53a56991edf7f851223171e1fd870c561085cd6c
Jawed Alam <zjaweds@outlook.com>
2021-08-15 00:12:27 +0530

    Face and eye detection implementation

--- faceDetection.py
+++ faceDetection.py
@@ -19,7 +19,7 @@ while True:
                                         minSize = (60,60), 
                                         flags = cv.CASCADE_SCALE_IMAGE)
     for (x,y,w,h) in faces:
-        # cv.rectangle(frame, (x,y),(x+w, y+h), (0,255,0),2)
+        cv.rectangle(frame, (x,y),(x+w, y+h), (0,255,0),2)
         faceROI = frame[y:y+h,x:x+w]
         eyes = eyeCascade.detectMultiScale(faceROI)
         for (x2, y2, w2, h2) in eyes:
